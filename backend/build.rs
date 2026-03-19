b59319f17147e0070ff32eaad52055a95ac397a7
petscheit <etscheitp@gmail.com>
2026-03-19 12:29:53 +0100

    chore: checkin compiled elf

--- backend/build.rs
+++ backend/build.rs
@@ -1,5 +1,14 @@
-use sp1_build::build_program_with_args;
-
 fn main() {
-    build_program_with_args("../program", Default::default())
+    let manifest_dir =
+        std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").expect("backend manifest dir"));
+    let elf_path = manifest_dir
+        .join("../program/elf/world-id-root-replicator-program")
+        .canonicalize()
+        .expect("checked-in SP1 ELF");
+
+    println!("cargo:rerun-if-changed={}", elf_path.display());
+    println!(
+        "cargo:rustc-env=SP1_ELF_world-id-root-replicator-program={}",
+        elf_path.display()
+    );
 }
